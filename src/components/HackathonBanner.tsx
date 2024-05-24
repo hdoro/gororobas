8fda2390ca4fe1c9cb091470cf23503c83c374ee
Henrique Doro <opensource@hdoro.dev>
2024-05-24 17:55:11 -0300

    feat: /hackathon redirect

--- src/components/HackathonBanner.tsx
+++ src/components/HackathonBanner.tsx
@@ -2,7 +2,7 @@ import { ExternalLinkIcon } from 'lucide-react'
 
 export default function HackathonBanner() {
 	return (
-		<div className="bg-secondary-200 px-pageX py-2 text-lg text-center sticky top-0 text-secondary-900">
+		<div className="bg-secondary-200 px-pageX py-2 text-lg text-center sticky top-0 text-secondary-900 z-50">
 			Coming from the EdgeDB Hackathon?{' '}
 			<a
 				href={'https://hdoro.mmm.page/edgedb-hackathon'}
