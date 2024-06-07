b9280a7edef4920db024df1b83cdabe2c4933eda
Henrique Doro <opensource@hdoro.dev>
2024-06-07 12:53:28 -0300

    fix: clean-up test in NewVegetableForm

--- src/app/vegetais/novo/NewVegetableForm.tsx
+++ src/app/vegetais/novo/NewVegetableForm.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { addVegetable } from '@/actions/addVegetable'
 import { createVegetableAction } from '@/actions/createVegetable.action'
-import { Button } from '@/components/ui/button'
 import dynamic from 'next/dynamic'
 
 // Skipping SSR to prevent hydration errors on react-hook-form changing IDs
@@ -12,11 +10,6 @@ const VegetableForm = dynamic(() => import('@/components/VegetableForm'), {
 
 export default function NewVegetableForm() {
 	return (
-		<>
-			<Button onClick={() => addVegetable()}>Create TEST Vegetable</Button>
-			<VegetableForm
-				onSubmit={(vegetable) => createVegetableAction(vegetable)}
-			/>
-		</>
+		<VegetableForm onSubmit={(vegetable) => createVegetableAction(vegetable)} />
 	)
 }
